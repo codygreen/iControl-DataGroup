c99f7b5b62f6ee0ddbbfab206c1a98d88f4b75dc
Cody Green <cody@codygreen.com>
2016-07-26 22:50:14 -0500

    removed debug statement
    removed debug statement and added standard function comments for self
    documentation.

--- datagroup.js
+++ datagroup.js
@@ -6,14 +6,24 @@ var bigip;
 // Constructor
 var dg = function(opts) {
   this.debug  = (typeof opts.debug  === 'boolean') ? opts.debug  : false;
-  // set API connection and authentication
+  // set API connection and authentication for iControl module
   bigip = new iControl(opts);
 };
 
+// static path for API access to data groups
 var dgPath = '/ltm/data-group/internal/';
 
+/**
+  * create data group
+  *
+  * @param {String} name of data group
+  * @param {String} type of data group (string, address, integer)
+  * @param {String} location of data group (internal, external)
+  * @param {String} parition of data group
+  */
 dg.prototype.create = function(name, type='string', location='internal',
   partition='Common', cb) {
+  // JSON object to create data group
   var dgData = {
     "name":name,
     "partition":partition,
@@ -26,9 +36,17 @@ dg.prototype.create = function(name, type='string', location='internal',
   });
 };
 
+/**
+  * delete data group
+  *
+  * @param {String} name of data group
+  * @param {String} location of data group (internal, external)
+  * @param {String} parition of data group
+  */
 dg.prototype.delete = function(name, location='internal', partition='Common', cb) {
+  // append the partition and dg name to the static path
   dgPath = dgPath+'~'+partition+'~'+name;
-  console.log('TEST: ', dgPath);
+  
   bigip.delete(dgPath, function(err, data) {
     if (err) throw err;
     cb(data);
